5a29b432519a50caa650d07a3384d4d3bb0ab719
ogbon <onethinneedful@gmail.com>
2020-09-11 19:43:45 +0100

    first commit

---
+++ util/database.js
@@ -0,0 +1,20 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'peaceHeart'
+// });
+
+// module.exports = pool.promise();
+
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'peaceHeart', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
